fe1a27ea9073c827e877554c22b3b30ebc6bf4d6
Wlodek Bzyl <matwb@univ.gda.pl>
2019-10-06 15:23:37 +0200

    do spec/strings_integers_rspec.rb

---
+++ lib/strings_integers.rb
@@ -0,0 +1,5 @@
+# frozen_string_literal: true
+
+def word_count(str)
+  str.split.size
+end
---
+++ spec/strings_integers_rspec.rb
@@ -0,0 +1,14 @@
+# frozen_string_literal: true
+
+require 'strings_integers'
+
+describe "#word_count" do
+  it "returns number of words in a string" do
+    string = "laughter, it's free"
+    expect(word_count(string)).to eq 3
+  end
+
+  it "returns 0 for empty string" do
+    expect(word_count("")).to eq 0
+  end
+end
